bb9fe86805fb57161e10bcb452b6c53a28fee1ff
namastegit <akshaykailey1507@gmail.com>
2024-04-01 11:38:11 +0530

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { Hono } from 'hono';
+import { sign } from 'hono/jwt'
+
+// Create the main Hono app
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string,
+		JWT_SECRET: string,
+	}
+}>();
+
+
+app.post('/api/v1/signup', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	try {
+		const user = await prisma.user.create({
+			data: {
+				email: body.email,
+				password: body.password
+			}
+		});
+		const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+		return c.json({ jwt });
+	} catch(e) {
+		c.status(403);
+		return c.json({ error: "error while signing up" });
+	}
+})
