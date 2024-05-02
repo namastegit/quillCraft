6bc06967d863efc8d9cc1e7b2d5e6fbeaa745a3c
namastegit <akshaykailey1507@gmail.com>
2024-05-02 11:53:18 +0530

    --

--- frontend/src/components/BlogSkeleton.tsx
+++ frontend/src/components/BlogSkeleton.tsx
@@ -2,7 +2,7 @@ import { Circle } from "./BlogCard"
 
 export const BlogSkeleton = () => {
     return <div role="status" className="animate-pulse">
-        <div className="p-5 border-b border-slate-200 pb-4 w-screen  cursor-pointer">
+        <div className="p-5 border-b border-slate-200 pb-4 w-screen  xl:max-w-screen-md cursor-pointer">
             <div className="flex">
                 <div className="h-4  bg-gray-200 rounded-full w-48 mb-4"></div>
                 <div className="h-2 bg-gray-200 rounded-full mb-2.5"></div>
